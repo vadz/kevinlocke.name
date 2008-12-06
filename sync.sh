777a3c97dd9a4d45091b31ff6a6c205abae7f289
Kevin Locke <kwl7@cornell.edu>
2008-12-06 15:02:54 -0700

    Synchronize all files with ftp server
    The --only-newer parameter was causing some files to be skipped during sync,
    so we remove it to make sure everything is synchronized.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-lftp -e "mirror -R --only-newer --no-perms --delete -x template\.php -x sync\.sh -x \.git -v && exit" -u kevinlo,25462e2 \
+lftp -e "mirror -R --no-perms --delete -x template\.php -x sync\.sh -x \.git -v && exit" -u kevinlo,25462e2 \
 	ftp://kevinlocke.name/public_html/testing
