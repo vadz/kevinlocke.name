06c5fba8447d6c8dcead4fb573aa3db1b673623c
Kevin Locke <kwl7@cornell.edu>
2008-12-06 13:37:24 -0700

    Modify sync.sh to not exclude testing
    In the testing branch, we don't exclude the testing directory so that if there
    is a testing directory in testing on the server we sync it (i.e. delete it)

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-lftp -e "mirror -R --only-newer --no-perms --delete -x testing -x template\.php -x sync\.sh -x \.git -v && exit" -u kevinlo,25462e2 \
-	ftp://kevinlocke.name/public_html
+lftp -e "mirror -R --only-newer --no-perms --delete -x template\.php -x sync\.sh -x \.git -v && exit" -u kevinlo,25462e2 \
+	ftp://kevinlocke.name/public_html/testing
