80cfcaada770b0ba5fb3464703037530409e9e50
Kevin Locke <kevin@kevinlocke.name>
2011-05-30 13:17:24 -0600

    Update sync script for new host
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- sync.sh
+++ sync.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-lftp -e "mirror -R --no-perms --delete -x template\.php -x sync\.sh -x \.git -v && exit" -u kevinlo,25462e2 \
-	ftp://kevinlocke.name/public_html/testing
+lftp -e "mirror -R --no-perms --delete -x template\.php -x sync\.sh -x \.git -v && exit" sftp://kevinlocke.name/srv/www/kevinlocke.name/testing
