85e3214e0aa52e647d3d8eb2174c89803a8b691b
Kevin Locke <kevin@kevinlocke.name>
2012-07-18 14:26:24 -0600

    Generate sitemap.xml
    As a minor assistance to crawlers.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- _bin/build.sh
+++ _bin/build.sh
@@ -10,6 +10,10 @@ for FILE in $(find _site/bits -name '*.html') ; do
     mv "$FILE" "${FILE%.html}.xhtml"
 done
 
+# Generate .html versions of .xhtml pages
 for FILE in $(find _site -name '*.xhtml') ; do
     xsltproc --nodtdattr -o "${FILE%.xhtml}.html" _bin/xhtmltohtml.xsl "$FILE"
 done
+
+# Remove .xhtml extension from URLs in the sitemap
+sed -i 's/\.xhtml<\/loc>/<\/loc>/' _site/sitemap.xml
