1004e5c3e6f47aa7f4da1475153c3f0aeb51daef
Kevin Locke <kevin@kevinlocke.name>
2012-08-25 14:56:17 -0600

    Be explicit about update times for posts
    Although using the mtime had a certain elegance, it was often unreliable
    (e.g. due to git branch changes) and didn't really match the semantic
    meaning of atom:updated and reader expectations, where an update
    includes significant changes.  Therefore, start specifying update times
    explicitly in the YML headers.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- _plugins/post-extras.rb
+++ _plugins/post-extras.rb
@@ -4,8 +4,9 @@ module Jekyll
     def to_liquid
       parts = content.split(/<!--\s*more\s*-->/, 2)
       original_to_liquid.deep_merge({
-        'excerpt' => if parts.length == 2 then parts[0] else nil end,
-        'mtime' => File.stat(File.join(@base, @name)).mtime
+        'excerpt' => if parts.length == 2 then parts[0] else nil end
+        # mtime often unreliable, currently unused
+        # 'mtime' => File.stat(File.join(@base, @name)).mtime
       })
     end
   end
