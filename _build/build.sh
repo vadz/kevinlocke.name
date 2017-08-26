0c1b0e9e03f281f142107fdb5bbf3ba79c04d13a
Kevin Locke <kevin@kevinlocke.name>
2017-08-26 16:36:37 -0600

    Create build-multiviews script
    This script creates HTML versions of XHTML files and .gz and .bro
    compressed versions of compressible files with names suitable for
    serving with Apache MultiViews.
    
    Make use of this script from build.sh.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- _build/build.sh
+++ _build/build.sh
@@ -3,24 +3,6 @@
 
 set -eux
 
-# trap wrappers since some shells don't call EXIT trap on signals (e.g. dash)
-# Use onexit "cmd" then unexit "cmd" around block to ensure cmd is always run
-unexit() { eval "$*"; trap "-" EXIT HUP INT KILL PIPE QUIT TERM; }
-onexit() { trap "$*; unexit" EXIT HUP INT KILL PIPE QUIT TERM; }
-
-# For determining appropriate levels of parallelism
-NPROCS=
-if [ -r /proc/cpuinfo ] ; then
-	NPROCS=$(grep -c ^processor /proc/cpuinfo)
-elif command -v sysctl >/dev/null ; then
-	NPROCS=$(sysctl -n hw.ncpu)
-fi
-
-if [ -z "$NPROCS" ] ; then
-	echo "Warning: Unable to detect number of CPU cores.  Guessing 2."
-	NPROCS=2
-fi
-
 if ! [ -d _build ] ; then
 	echo 'Error: Must be run from site directory with _build subdir.' >&2
 	exit 1
@@ -70,39 +52,10 @@ fi
 
 # Note:  Not checking .html files, which are phpdoc/javadoc
 
-# Generate .html versions of .xhtml pages
-find _site -name '*.xhtml' | while IFS= read -r FILE; do
-    xsltproc --nodtdattr -o "${FILE%.xhtml}.html" _build/xhtmltohtml.xsl "$FILE"
-    # Replace the over-conservative us-ascii charset with utf-8
-    perl -pi -e 's/\s*<meta[^>]*http-equiv="Content-Type"[^>]*>\s*/  <meta charset="utf-8" \/>\n/i' -- "${FILE%.xhtml}.html"
-done
-
 # Check other XML for well-formedness
 find _site \( -iname '*.atom' -o -iname '*.rss' -o -iname '*.xml' \) -print0 \
 	| xargs -0 -r xmllint --nonet --noout
 
-# Pre-compress common text files
-COMPRESSIBLE_FILES=$(mktemp -t compressible-files.XXXXXX)
-onexit rm "$COMPRESSIBLE_FILES"
-find _site \( -iname '*.atom' \
-	-o -iname '*.asc' \
-	-o -iname '*.css' \
-	-o -iname '*.html' \
-	-o -iname '*.js' \
-	-o -iname '*.pem' \
-	-o -iname '*.svg' \
-	-o -iname '*.txt' \
-	-o -iname '*.vcard' \
-	-o -iname '*.vcf' \
-	-o -iname '*.xhtml' \
-	-o -iname '*.xml' \
-	\) -print0 > "$COMPRESSIBLE_FILES"
-xargs -0 -r pigz -9 -k < "$COMPRESSIBLE_FILES"
-if command -v brotli ; then
-	xargs -0 -r '-I{}' -P$((NPROCS+2)) brotli --input '{}' --output '{}.bro' < "$COMPRESSIBLE_FILES"
-fi
-# Rename uncompressed files with double type extension so that MultiViews will
-# negotiate requests for the original file name.
-xargs -0 sh -c 'for FILE; do EXT=${FILE##*.}; mv "$FILE" "${FILE}.$EXT"; done' sh < "$COMPRESSIBLE_FILES"
+build-multiviews _site _site
 
 echo Done building site.
